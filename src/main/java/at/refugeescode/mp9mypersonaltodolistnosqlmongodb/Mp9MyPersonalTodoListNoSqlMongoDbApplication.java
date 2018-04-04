ba2332261e7632a1acb8f49a779e9a7481c7c13e
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-04-04 11:01:03 +0200

    Add project structure

---
+++ src/main/java/at/refugeescode/mp9mypersonaltodolistnosqlmongodb/Mp9MyPersonalTodoListNoSqlMongoDbApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp9mypersonaltodolistnosqlmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp9MyPersonalTodoListNoSqlMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp9MyPersonalTodoListNoSqlMongoDbApplication.class, args);
+	}
+}
